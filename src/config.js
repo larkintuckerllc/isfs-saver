b5f0815371f2f00bfb4c119d8956b4cbf2a9b2c3
John Tucker <john@larkintuckerllc.com>
2016-10-13 09:06:28 -0400

    Fixed Bug

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const ACTION_PREFIX = 'app/';
 const BASE = `${window.location.protocol}//${window.location.hostname}`;
 export const BASE_URL_HTTP = `${BASE}:3000`;
 export const BASE_URL_SOCKET = `${BASE}:3001`;
-export const CHANNELS = [0, 1];
+export const CHANNELS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 export const MATRIX = [
   [0, 1, 2],
   [3, 4, 5],
